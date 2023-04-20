addc09668772ee4ff92d833ce82fc9876eacc9be
olgazh565 <olgazh565@gmail.com>
2023-04-20 23:27:07 +0300

    task complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const swiper = new Swiper('.swiper', {
     
     slidesPerView: 1,
-    loop: true,
     navigation: {
         nextEl: '.advantages__slider-btn_next',
         prevEl: '.advantages__slider-btn_prev',
